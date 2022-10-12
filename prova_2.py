a615d735dceb2af02615d8622bdd4d549297f505
lollo2 <lolloferri92@gmail.com>
2022-10-12 16:14:56 +0200

    questo è il commit di prova per il primo file di python cresto

---
+++ prova_2.py
@@ -0,0 +1,25 @@
+a=12
+print (a)
+
+
+a="cazzo mi frega dei due tuoi"
+print(a)
+
+nome = input('Inserisci il tuo nome: ')
+print(nome)
+
+raggio = input('inserisci il raggio:')
+r=float (raggio)
+print(raggio,r)
+
+area=r*2
+print(area)
+print(raggio*4)
+
+i=3
+if (i==2):
+    print('l\'identazione funziona')
+else:(print('cass'))
+
+#questo è il primo esempio di commento
+#piu commenti vanno su più righe se ogni volta metto di nuovo l'asterisco
